dafcb0545c227d1835f9b699b81a94d596c28a6d
falgunkk <falgunbalachandran@gmail.com>
2022-03-03 22:51:16 +0530

    rails from 6.1.4.6 to 7.00, ruby 2.6.6 to 2.7.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_155933) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_03_03_171045) do
   create_table "active_storage_blobs", charset: "utf8mb4", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
@@ -19,8 +18,8 @@ ActiveRecord::Schema.define(version: 2022_03_02_155933) do
     t.text "metadata"
     t.string "service_name", null: false
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.string "checksum"
+    t.datetime "created_at", precision: nil, null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true, length: 191
   end
 
@@ -34,10 +33,10 @@ ActiveRecord::Schema.define(version: 2022_03_02_155933) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "role"
   end
 
